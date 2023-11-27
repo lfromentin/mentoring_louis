7c9c75860365647afa3517128aaba77c53fb9bb6
lfromentin <151257033+lfromentin@users.noreply.github.com>
2023-11-27 17:27:08 +0000

    Co-authored-by: Antoine <bigott-a@users.noreply.github.com>

---
+++ over_the_moon/meteor_showers/meteor_showers.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+meteor_showers = pd.read_csv('data/meteorshowers.csv')
+moon_phases = pd.read_csv('data/moonphases.csv')
+constellations = pd.read_csv('data/constellations.csv')
+cities = pd.read_csv('data/cities.csv')
+
+print("break")
